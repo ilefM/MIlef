ecda70ea4a3ae31ab75d3e2ec9181f326a158ea0
ilefM <ilefikhelefdev1@gmail.com>
2024-06-27 15:40:47 -0400

    project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <>
-      <h1 className="text-red-500 text-3xl">Mohammed Ilef Ikhelef</h1>
-    </>
+    <div>
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div>Ilef</div>;
+}
+
+export default Header;
