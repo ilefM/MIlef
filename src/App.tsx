dd7473eabb72d8ec4380994e2b60561a8e77ab70
ilefM <ilefikhelefdev1@gmail.com>
2024-07-19 15:43:00 -0400

    adjusting projects section layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import AboutMe from './components/AboutMe';
 import Header from './components/Header';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <div className="w-full mx-auto px-4 max-w-[900px] mt-20 flex flex-col text-textColor dark:text-slate-50 font-dosis">
         <AboutMe />
+        <Projects />
       </div>
     </Suspense>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,6 @@
 import fikra from '../assets/fikra.png';
 import sortingVisualiser from '../assets/sortingVisualiser.png';
 import passwordGenerator from '../assets/passwordGenerator.png';
-import { FaGithub } from 'react-icons/fa';
-import { FaExternalLinkAlt } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 
 function Projects() {
@@ -33,48 +31,23 @@ function Projects() {
   ];
 
   return (
-    <div className="bg-neutral-100 w-full border-2">
-      <div className="mt-32 mx-8">
-        <div className="flex opacity-45 mb-4">
-          <div className="bg-gray-400 dark:text-[#fefae0] mt-4 w-full h-[1px]"></div>
-          <p className="mx-4 text-xl font-dosisSemiBold text-neutral-800 dark:text-[#fefae0] text-nowrap text-center">
-            {t('petProjects.title')}
-          </p>
-          <div className="bg-gray-400 dark:text-[#fefae0] mt-4 w-full h-[1px]"></div>
-        </div>
-        <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-between -mx-2">
-          {projects.map((p) => (
-            <div
-              className="flex flex-col p-2 m-2 w-80 max-w-full h-60 bg-white rounded-sm shadow-sm hover:shadow-2xl transition-shadow duration-300 ease-in-out"
-              key={p.id}
-            >
-              <div className="flex p-2 items-center justify-center bg-gray-200 w-full h-2/3 rounded-sm overflow-hidden">
-                <img src={p.url} className="w-4/5" />
-              </div>
-              <div className="mt-2">
-                <h2 className="text-lg mb-2">{p.title}</h2>
-                <div className="flex space-x-4">
-                  <a
-                    href={p.github}
-                    className="text-neutral-500"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGithub />
-                  </a>
-                  <a
-                    href={p.live}
-                    className="text-neutral-500"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaExternalLinkAlt />
-                  </a>
-                </div>
-              </div>
+    <div className="mt-16 w-full">
+      <p className="text-2xl font-dosis text-textColor dark:text-slate-50 text-nowrap">
+        {t('petProjects.title')}
+      </p>
+      <p className="text-sm italic">{t('petProjects.indication')}</p>
+      <div className="flex flex-col sm:flex-row items-center mt-4 justify-center sm:justify-between -mx-2">
+        {projects.map((p) => (
+          <div
+            className="flex flex-col m-2 w-64 max-w-full h-52 bg-foreground dark:bg-darkForeground rounded-lg shadow-lg overflow-hidden"
+            key={p.id}
+          >
+            <img src={p.url} className="w-full h-36 object-contain p-4" />
+            <div className="px-4">
+              <h2 className="text-lg">{p.title}</h2>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
