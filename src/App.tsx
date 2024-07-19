fd794d68abe46eeee9dfe648bc55267a94653ff5
ilefM <ilefikhelefdev1@gmail.com>
2024-07-19 12:52:16 -0400

    fixing lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 import AboutMe from './components/AboutMe';
 import Header from './components/Header';
-import Projects from './components/Projects';
 
 function App() {
   return (
