aba2154fee9865d629e42677eeebb767071a2b1d
ilefM <ilefikhelefdev1@gmail.com>
2024-07-20 23:57:52 -0400

    fixing alignement

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Projects from './components/Projects';
 function App() {
   return (
     <Suspense fallback="loading">
-      <Header />
-      <div className="w-full mx-auto px-4 max-w-[900px] mt-20 flex flex-col text-textColor dark:text-slate-50 font-dosis">
-        <AboutMe />
-        <Projects />
+      <div className="w-full max-w-[800px] mx-auto px-4">
+        <Header />
+        <div className="mt-40 flex flex-col text-textColor dark:text-slate-50 font-dosis">
+          <AboutMe />
+          <Projects />
+        </div>
       </div>
     </Suspense>
   );
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,16 +9,16 @@ function AboutMe() {
 
   return (
     <div className="flex flex-col mx-auto items-center w-full">
-      <div className="w-full max-w-[620px] flex flex-col sm:flex-row sm:justify-between  items-center mt-4">
+      <div className="w-full flex flex-col sm:flex-row sm:justify-between  items-center mt-4">
         <img
-          className="order-1 sm:order-2 w-[170px] shadow-lg rounded-full"
+          className="order-1 sm:order-2 w-[170px] sm:w-[260px] shadow-lg rounded-full"
           src={profilepic}
           alt="profile picture"
         />
-        <div className="order-2 sm:order-1 flex flex-col items-center sm:items-start">
+        <div className="order-2 sm:order-1 flex flex-col items-center sm:items-start sm:justify-between">
           <p className="text-xl mt-8 sm:text-4xl">Mohammed Ilef</p>
           <h1 className="text-7xl sm:text-9xl font-dosis">Ikhelef</h1>
-          <div className="flex space-x-4 mt-4">
+          <div className="order-3 flex space-x-4 mt-4">
             <div className="bg-foreground dark:bg-darkForeground rounded-md p-1">
               <a
                 href="https://github.com/ilefM"
@@ -50,13 +50,6 @@ function AboutMe() {
         </div>
       </div>
 
-      {/* <div className="border-l-2 dark:border-slate-50 mx-auto mt-14">
-        <div className="ml-2 space-y-2">
-          <p>Developer</p>
-          <p>Geek</p>
-          <p>Developer</p>
-        </div>
-      </div> */}
       <div className="mt-32 sm:mt-28">
         <h2 className="text-2xl">{t('about.title')}</h2>
         <div className="mt-2 bg-foreground dark:bg-darkForeground rounded-md p-4">
