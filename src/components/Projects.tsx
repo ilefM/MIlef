f57a461a376cff4bf83fb628226ff44294f4ddb7
ilefM <ilefikhelefdev1@gmail.com>
2024-07-30 14:00:27 -0400

    adjusting alignement and updating pdfs

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -16,13 +16,13 @@ function Projects() {
         {t('petProjects.seeMore')}
       </Link>
       <div className="flex flex-col sm:flex-row items-center mt-2 justify-center sm:justify-between -mx-2">
-        <div className="flex flex-col m-2 w-64 max-w-full h-48 bg-foreground dark:bg-darkForeground rounded-lg shadow-lg overflow-hidden hover:shadow-xl">
+        <div className="flex flex-col m-2 w-64 max-w-full h-52 bg-foreground dark:bg-darkForeground rounded-lg shadow-lg overflow-hidden hover:shadow-xl">
           <img src={fikra} className="w-full h-36 object-contain p-4" />
           <div className="px-4">
             <h2 className="text-lg">{t('petProjects.fikra.title')}</h2>
           </div>
         </div>
-        <div className="flex flex-col m-2 w-64 max-w-full h-48 bg-foreground dark:bg-darkForeground rounded-lg shadow-lg overflow-hidden hover:shadow-xl">
+        <div className="flex flex-col m-2 w-64 max-w-full h-52 bg-foreground dark:bg-darkForeground rounded-lg shadow-lg overflow-hidden hover:shadow-xl">
           <img
             src={sortingVisualiser}
             className="w-full h-36 object-contain p-4"
@@ -33,7 +33,7 @@ function Projects() {
             </h2>
           </div>
         </div>
-        <div className="flex flex-col m-2 w-64 max-w-full h-48 bg-foreground dark:bg-darkForeground rounded-lg shadow-lg overflow-hidden hover:shadow-xl">
+        <div className="flex flex-col m-2 w-64 max-w-full h-52 bg-foreground dark:bg-darkForeground rounded-lg shadow-lg overflow-hidden hover:shadow-xl">
           <img
             src={passwordGenerator}
             className="w-full h-36 object-contain p-4"
