716c4ce97151c059c5a45961f9e492262570a4df
ilefM <ilefikhelefdev1@gmail.com>
2024-11-11 22:40:20 -0500

    adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx';
 import './index.css';
 import './i18n/i18n.ts';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics />
   </React.StrictMode>
 );
